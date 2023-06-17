c7730a891c797a42b9dcbf138214b76f4af2d634
Shubham Chaudhari <101280545+ShubhamChaudhari9990@users.noreply.github.com>
2023-06-17 23:13:19 +0530

    Authentication

---
+++ authentication/src/app/pages/home/home.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from "@angular/fire/compat/firestore"
+import { MatDialog } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
+import { AddRecordsComponent } from 'src/app/components/add-records/add-records.component';
+import { UpdateRecordComponent } from 'src/app/components/update-record/update-record.component';
+
+// export interface PeriodicElement {
+//   name: string;
+//   position: number;
+//   weight: number;
+//   symbol: string;
+// }
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  // displayedColumns: string[] = ['position', 'name', 'weight', 'symbol','actions'];
+  displayedColumns: string[] = ['name', 'address', 'contact', 'qualification', 'action'];
+
+  dataSource = [] as any
+  constructor(private afs: AngularFirestore, private dailog: MatDialog) {
+
+  }
+  ngOnInit(): void {
+    this.afs.collection("student").valueChanges({ idField: 'id' }).subscribe((res) => {
+      this.dataSource = res      
+    })
+  }
+
+  applyFilter(event: Event) {
+    
+    this.afs.collection("student").valueChanges({ idField: 'id' }).subscribe((res) => {
+      const data = new MatTableDataSource(this.dataSource);    
+      const filterValue = (event.target as HTMLInputElement).value;
+      data.filter = filterValue.trim().toLowerCase();
+      console.log(data);    })
+    
+  }
+
+  addRecord(): void {
+    const ref = this.dailog.open(AddRecordsComponent);
+    ref.afterClosed().subscribe(res => {
+      if (res) {
+        this.afs.collection("student").add(res)        
+      }
+    });
+  }
+  delete(row: any) {
+    this.afs.collection("student").doc(row.id).delete()
+  }
+  update(row : any){
+    const updateRef = this.dailog.open(UpdateRecordComponent,{
+      data: {row}
+    });
+    updateRef.afterClosed().subscribe(res => {
+      this.afs.collection("student").doc(row.id).update(res)
+    })
+  }
+}
